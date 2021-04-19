029e8441140ba52dc9aeaa3851fe24c3102475a8
Michal Malujdy <mima@scalepoint.com>
2021-04-19 17:15:02 +0200

    Fix mobile styling

--- src/components/Formatter/Formatter.js
+++ src/components/Formatter/Formatter.js
@@ -18,7 +18,7 @@ class Formatter extends React.Component {
     render() {
         return (
             <div className="formatter">
-                <div>
+                <div className="formatter_content">
                     <FormatterForm onSubmit={this.onFormSubmit}/>
                     
                     <Card title="Branch name" extra={this.renderCopyToClipboard()}>
